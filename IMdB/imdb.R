5a5d13168440ddbad40be09cb8a83686315283de
Tharunraj <110415018+Tharunraj-s@users.noreply.github.com>
2022-11-14 13:23:21 +0530

    Update imdb.R

--- IMdB/imdb.R
+++ IMdB/imdb.R
@@ -1,22 +1,21 @@
 library(robotstxt)
 library(rvest)
+url <- "https://www.imdb.com/search/title/?groups=top_250&sort=user_rating"
+path = paths_allowed(url)
+#html from website
+web = read_html(url)
+View(web)
+names <- web %>% html_nodes(".lister-item-header a")%>% html_text()
+View(names)
+year <- web %>%html_nodes(".text-muted.unbold") %>% html_text()
+View(year)
+duration <- web%>%html_nodes(".runtime") %>% html_text()
+View(duration)
+rating <- web %>% html_nodes(".ratings-imdb-rating strong") %>% html_text()
+View(rating)
 
-Path= paths_allowed("https://www.imdb.com/chart/top/")
-
-url<-"https://www.imdb.com/chart/top/"
-
-my_html<-read_html(url)
-
-my_tables <- html_nodes(my_html,"table")
-
-movie_table <- html_table(my_tables)[(1)]
-
-View(movie_table)
-
-movies <- movie_table[,2:3]
-
-View(movies)
-
-str(movies)
-
-write.csv(movies,"my_movies.csv")
+#creating dataframe
+imdb <- data.frame(names,year,duration,rating)
+View(imdb)
+#save
+write.csv(imdb,"movieimdb.csv")
